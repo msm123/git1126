387f0ddd438034ea30d0c4183bebe47ce40cf758
style_ma <style_ma@163.com>
2022-04-16 19:39:54 +0800

    third commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -10,6 +10,7 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("git test");
         System.out.println("git test 2");
+        System.out.println("git test 3");
     }
 }
 
