576dd6d0f6b26c28ff48845c36d22f2c254c4aea
style_ma <style_ma@163.com>
2022-04-16 19:34:43 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,15 @@
+package com.atguigu.git;
+
+/**
+ * @title: GitTest
+ * @Author msm
+ * @Date: 2022/4/16 18:22
+ * @Version 2018.3.6
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("git test");
+    }
+}
+
+
